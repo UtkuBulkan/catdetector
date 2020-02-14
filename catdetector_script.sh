b3c5f1403261abd6086d02ebd8f0eba4c9517a84
bulkanutku@gmail.com <bulkanutku@gmail.com>
2020-02-14 22:56:27 +0000

    Added app credentials env value

--- catdetector_script.sh
+++ catdetector_script.sh
@@ -1,3 +1,4 @@
+export GOOGLE_APPLICATION_CREDENTIALS='/catdetector_gcloud_certificates/service-cert/supereye.co.uk-storage.json'
 gcloud auth activate-service-account --key-file /catdetector_gcloud_certificates/service-cert/supereye.co.uk-storage.json
 gcsfuse --implicit-dirs supereye.co.uk /catdetector/release/data
 cd /catdetector/release 
