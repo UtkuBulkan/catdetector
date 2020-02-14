8911af8712aad635d9dbd7a90baaf732e6f60181
bulkanutku@gmail.com <bulkanutku@gmail.com>
2020-02-14 22:18:32 +0000

    Updated dockerfile with gcloud storage initialization and startup script

---
+++ catdetector_script.sh
@@ -0,0 +1,5 @@
+gcloud auth activate-service-account --key-file /catdetector_gcloud_certificates/service-cert/supereye.co.uk-storage.json
+gcsfuse --implicit-dirs supereye.co.uk /catdetector/release/data
+cd /catdetector/release 
+#./catdetector data/bulkanutku-1/birds.mp4/birds.mp4 birds.mp4 bulkanutku-1
+./catdetector $catdetector_path $catdetector_uuid $catdetector_user
